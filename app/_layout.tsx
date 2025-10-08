028e14f22d3ada7740025f6316d6ad324d4c9e0b
Julia Lyngfelt <julialyngfelt@hotmail.com>
2025-10-08 15:22:06 +0200

    11 add auth registrations (#16)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,23 +4,31 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { createTamagui,TamaguiProvider } from 'tamagui';
+import { defaultConfig } from '@tamagui/config/v4';
+
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
-//ersätt fil med eget innehåll
+const config = createTamagui(defaultConfig)
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+
+
   return (
+
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <TamaguiProvider config={config}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
+      </TamaguiProvider>
     </ThemeProvider>
   );
 }
