07dae10687250b30e42474d14287c81f073e0104
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-10-13 14:50:27 +0200

    added toggle w/ true/false and foundation styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,29 @@
-import { Platform, StyleSheet, Text, Button } from 'react-native';
-import { useRouter } from 'expo-router';
-import TabLayout from './_layout';
-import { useAuth } from '../../contexts/AuthContext';
-import { DefaultButton } from '@/components/ui/buttons/DefaultButton';
-
+import { DefaultButton } from "@/components/ui/buttons/DefaultButton";
+import { useRouter } from "expo-router";
+import { StyleSheet, Text } from "react-native";
+import { useAuth } from "../../contexts/AuthContext";
+import TabLayout from "./_layout";
+import { DefaultSwitch } from "@/components/ui/switch/DefaultSwitch";
 
 export default function ProfileScreen() {
+  const router = useRouter();
+  const { user, loading } = useAuth();
 
-    const router = useRouter();
-      const { user, loading } = useAuth();
+
   return (
     <>
-    <Text 
-    style={{fontSize: 50, padding: 40}}>PROFIL</Text>
-    <Text style={{fontSize: 50, padding: 40}} >Email: {user?.email}</Text>
+      <Text style={{ fontSize: 50, padding: 40 }}>PROFIL</Text>
+      <Text style={{ fontSize: 50, padding: 40 }}>Email: {user?.email}</Text>
 
-    <TabLayout/>
-          <DefaultButton
-            onPress={() => router.replace("/edit-profile")}
-            variant="primary"
-          > Redigera profil</DefaultButton>
-
+      <TabLayout />
+      <DefaultButton onPress={() => router.replace("/edit-profile")}>
+        {" "}
+        Redigera profil
+      </DefaultButton>
+      <DefaultSwitch/>
 
     </>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
