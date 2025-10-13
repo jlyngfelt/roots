71abc2c5818fc3cd58fb4b4d92da0f7fae8b628f
anna-dahlberg <anna.dahlberg@live.se>
2025-10-13 11:58:16 +0200

    Added storage

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,26 +1,25 @@
-import { getFirestore } from "firebase/firestore";
-import { initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { initializeApp } from 'firebase/app'
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6SCHQ-4fUscJS8-UWMJdngtqFQtsJ0zI",
-  authDomain: "roots-cd39a.firebaseapp.com",
-  projectId: "roots-cd39a",
-  storageBucket: "roots-cd39a.firebasestorage.app",
-  messagingSenderId: "872631865213",
-  appId: "1:872631865213:web:f134fe9fb5cdf2fe4c0614",
-  measurementId: "G-YNGG0LDKX4"
-};
+	apiKey: 'AIzaSyD6SCHQ-4fUscJS8-UWMJdngtqFQtsJ0zI',
+	authDomain: 'roots-cd39a.firebaseapp.com',
+	projectId: 'roots-cd39a',
+	storageBucket: 'roots-cd39a.firebasestorage.app',
+	messagingSenderId: '872631865213',
+	appId: '1:872631865213:web:f134fe9fb5cdf2fe4c0614',
+	measurementId: 'G-YNGG0LDKX4'
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
-// Exportera de tjänster du behöver
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
-});
-
-export const db = getFirestore(app);
-export default app;
+	persistence: getReactNativePersistence(AsyncStorage)
+})
+export const db = getFirestore(app)
+export const storage = getStorage(app)
+export default app
