a0f26830c811c6f32c09e5930c9ee8c5214d78a5
Julia Lyngfelt <julialyngfelt@hotmail.com>
2025-10-14 11:22:24 +0200

    added edit profile logic no image upload (#54)
    LGTM!

--- app/edit-profile.tsx
+++ app/edit-profile.tsx
@@ -1,14 +1,47 @@
 // här ändrar man ex sin bio, sin profilbild och kanske mer. När man trycker på spara = (router.replace('/(tabs)'))
 
 import { useRouter } from "expo-router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TextInput, View } from "react-native";
 import { Text } from "tamagui";
+import { DefaultButton } from "@/components/ui/buttons/DefaultButton";
+import { DefualtInput } from "@/components/ui/input/DefaultInput";
+import { DefualtTextArea } from "@/components/ui/input/DefaultTextArea";
+import { updateUserProfile, getUserProfile } from "@/services/userService";
+import { useAuth } from "@/contexts/AuthContext";
 
 export default function LoginScreen() {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { user } = useAuth();
+  const [newUsername, setNewUsername] = useState("");
+  const [newPostalcode, setNewPostalcode] = useState("");
+  const [newBio, setNewBio] = useState("");
+
+
+  
+  useEffect(() => {
+    if (user?.uid) {
+      async function fetchProfile() {
+        const profile: any = await getUserProfile(user?.uid);
+        if (profile) {
+          setNewUsername(profile.username || "");
+          setNewPostalcode(profile.postalCode || "");
+          setNewBio(profile.bio || "");
+        }
+      }
+      fetchProfile();
+    }
+  }, [user?.uid]);
+
+
+  async function handleSave() {
+    await updateUserProfile(user?.uid, { 
+      username: newUsername,
+      postalCode: newPostalcode,
+      bio: newBio
+    });
+    router.replace('/(tabs)');
+  }
 
   return (
     <View
@@ -20,24 +53,26 @@ export default function LoginScreen() {
         alignItems: "center",
       }}
     >
-      <Text style={{ fontSize: 20, color: "green" }}>ROOTS</Text>
+{/* Här ska sedan finnas ett state om bild ej finns skriv ladda upp */}
+<DefaultButton variant="tertiary">{"Ändra profilbild"}</DefaultButton> 
 
-      <TextInput
-        value={email}
-        onChangeText={setEmail}
-        placeholder="email"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        style={{ height: 40, fontSize: 30, color: "black" }}
+      <DefualtInput 
+        value={newUsername}
+        onChangeText={setNewUsername}
+        placeholder="Användarnamn"
       />
-      <TextInput
-        value={password}
-        onChangeText={setPassword}
-        placeholder="lösenord"
-        secureTextEntry={true}
-        autoCapitalize="none"
-        style={{ height: 40, fontSize: 30, color: "black" }}
+      <DefualtInput 
+        value={newPostalcode}
+        onChangeText={setNewPostalcode}
+        placeholder="Postnummer"
       />
+      <DefualtTextArea 
+        value={newBio}
+        onChangeText={setNewBio}
+        placeholder="Bio"
+      />
+      <DefaultButton onPress={handleSave}>Spara</DefaultButton>
+
     </View>
   );
 }
