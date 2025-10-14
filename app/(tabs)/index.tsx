8536025f289664bf5e47f1f6bf117744100d7c7e
anna-dahlberg <anna.dahlberg@live.se>
2025-10-14 15:34:04 +0200

    added image preview and upload to profile settings

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { DefaultSwitch } from "@/components/ui/switch/DefaultSwitch";
 import { getUserProfile } from "@/services/userService";
 import { useRouter } from "expo-router";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { useAuth } from "../../contexts/AuthContext";
 import TabLayout from "./_layout";
 
@@ -38,16 +38,47 @@ export default function ProfileScreen() {
 
   return (
     <>
-      <Text style={{ fontSize: 50, padding: 40 }}>PROFIL</Text>
-      <Text style={{ fontSize: 50, padding: 40 }}>Email: {user?.email}</Text>
-      <Text style={{ fontSize: 20, padding: 40 }}>
+      <Text style={{ fontSize: 20, padding: 10 }}>PROFIL</Text>
+
+      {/* Profile Image */}
+      {userProfile?.profileImageUrl ? (
+        <Image
+          source={{ uri: userProfile.profileImageUrl }}
+          style={{
+            width: 240,
+            height: 240,
+            padding: 40,
+            borderRadius: 16,
+            alignSelf: "center",
+            marginBottom: 24,
+          }}
+        />
+      ) : (
+        <View
+          style={{
+            width: 240,
+            height: 240,
+            padding: 40,
+            borderRadius: 16,
+            marginBottom: 24,
+            backgroundColor: "gray",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "white" }}>Ingen bild</Text>
+        </View>
+      )}
+
+      <Text style={{ fontSize: 20, padding: 10 }}>Email: {user?.email}</Text>
+      <Text style={{ fontSize: 20, padding: 10 }}>
         username: {userProfile?.username}
       </Text>
-      <Text style={{ fontSize: 20, padding: 40 }}>
+      <Text style={{ fontSize: 20, padding: 10 }}>
         postal: {userProfile?.postalCode}
       </Text>
-      <Text style={{ fontSize: 20, padding: 40 }}>bio: {userProfile?.bio}</Text>
-      <Text style={{ fontSize: 20, padding: 40 }}>
+      <Text style={{ fontSize: 20, padding: 10 }}>bio: {userProfile?.bio}</Text>
+      <Text style={{ fontSize: 20, padding: 10 }}>
         credits: {userProfile?.credits}
       </Text>
 
