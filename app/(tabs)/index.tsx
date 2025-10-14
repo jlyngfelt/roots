1e32b943257ac612fcde6afec029714596bd1b4a
Julia Lyngfelt <julialyngfelt@hotmail.com>
2025-10-14 10:46:41 +0200

    displaying profile info on profile page (#53)
    LGTM!

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
 import { DefaultButton } from "@/components/ui/buttons/DefaultButton";
 import { DefaultSwitch } from "@/components/ui/switch/DefaultSwitch";
+import { getUserProfile } from "@/services/userService";
 import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text } from "react-native";
 import { useAuth } from "../../contexts/AuthContext";
 import TabLayout from "./_layout";
@@ -8,11 +10,46 @@ import TabLayout from "./_layout";
 export default function ProfileScreen() {
   const router = useRouter();
   const { user, loading } = useAuth();
+  const [userProfile, setUserProfile] = useState<Partial<UserProfile> | null>(
+    null
+  );
+
+  interface UserProfile {
+    id: string;
+    username: string;
+    postalCode: string;
+    bio: string;
+    profileImageUrl: string;
+    credits: number;
+    createdAt: any;
+  }
+
+  useEffect(() => {
+    if (user?.uid) {
+      async function fetchProfile() {
+        const profile = await getUserProfile(user?.uid);
+        if (profile) {
+          setUserProfile(profile);
+        }
+      }
+      fetchProfile();
+    }
+  }, [user?.uid]);
 
   return (
     <>
       <Text style={{ fontSize: 50, padding: 40 }}>PROFIL</Text>
       <Text style={{ fontSize: 50, padding: 40 }}>Email: {user?.email}</Text>
+      <Text style={{ fontSize: 20, padding: 40 }}>
+        username: {userProfile?.username}
+      </Text>
+      <Text style={{ fontSize: 20, padding: 40 }}>
+        postal: {userProfile?.postalCode}
+      </Text>
+      <Text style={{ fontSize: 20, padding: 40 }}>bio: {userProfile?.bio}</Text>
+      <Text style={{ fontSize: 20, padding: 40 }}>
+        credits: {userProfile?.credits}
+      </Text>
 
       <TabLayout />
       <DefaultButton onPress={() => router.replace("/settings")}>
