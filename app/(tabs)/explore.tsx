12f12dc35797e4a8f45f4dd9b28c6ee659bb80cc
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-10-14 15:29:17 +0200

    created foundation for explore feed

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,11 +1,58 @@
-import { Platform, StyleSheet, Text } from "react-native";
+import { getOtherUsersPlants } from "@/services/plantService";
 import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, ScrollView, View } from "react-native";
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function ExploreScreen() {
   const router = useRouter();
+  const { user } = useAuth();
+  const [plants, setPlants] = useState<Plant[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  interface Plant {
+    id: string;
+    name: string;
+    description?: string;
+    readyToAdopt?: boolean;
+    // fyll på här med resten av våra fält
+  }
+
+  useEffect(() => {
+    if (user?.uid) {
+      async function fetchPlants() {
+        try {
+          setLoading(true);
+          const otherPlants = await getOtherUsersPlants(user?.uid);
+          setPlants(otherPlants);
+        } catch (err) {
+          setError(err.message);
+          console.error("Error fetching plants:", err);
+        } finally {
+          setLoading(false);
+        }
+      }
+      fetchPlants();
+    } else {
+      setLoading(false);
+    }
+  }, [user?.uid]);
+
   return (
     <>
+    <ScrollView>
+
       <Text style={{ fontSize: 50, padding: 40 }}>EXPLORE</Text>
+      
+        {plants.map((plant) => (
+          <View key={plant.id}>
+            <Text>{plant.name}</Text>
+            <Text>Ready to adopt: {plant.readyToAdopt ? "Yes" : "No"}</Text>
+          </View>
+        ))}
+      
+        </ScrollView>
     </>
   );
 }
