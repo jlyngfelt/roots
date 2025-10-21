f1978c9ebd956c0637a830e2fcf3f818e43ef3e1
anna-dahlberg <anna.dahlberg@live.se>
2025-10-21 14:32:14 +0200

    moved to new folder

--- components/ui/form/DefaultForm.tsx
+++
@@ -1,4 +0,0 @@
-import { Colors } from "@/constants/design-system";
-import { useState } from "react";
-import { DefaultInput } from "./DefaultInput";
-import { DefaultTextArea } from "./DefaultTextArea";
