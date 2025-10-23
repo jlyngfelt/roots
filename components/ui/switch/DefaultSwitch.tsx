3a8d3f3ce459abca4ac4f179401448459acd07d6
Julia Lyngfelt <julialyngfelt@hotmail.com>
2025-10-23 16:30:00 +0200

    Feature: Style toggle button (#168)

--- components/ui/switch/DefaultSwitch.tsx
+++ components/ui/switch/DefaultSwitch.tsx
@@ -1,12 +1,14 @@
 import { Switch, SwitchProps } from "tamagui";
+import { Colors } from "../../../constants/design-system"
 
 export const DefaultSwitch = (props: SwitchProps) => {
   return (
     <Switch
       size="$4"
+      width={80}
       defaultChecked={false}
       {...props}
-      backgroundColor="$green10"
+      backgroundColor={props.checked ? Colors.accent : Colors.grey}
     >
       <Switch.Thumb animation="bouncy" backgroundColor="white" />
     </Switch>
