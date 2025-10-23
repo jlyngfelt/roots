ff3d9ce088701ed88ff863655f8dad2b80dd1250
Anna Dahlberg <anna.dahlberg@live.se>
2025-10-23 16:55:26 +0200

    Fix: Import issues (#171)

--- app/view-plant/[plantId].tsx
+++ app/view-plant/[plantId].tsx
@@ -1,13 +1,17 @@
 import { DefaultButton } from "@/components/ui/buttons/DefaultButton";
 import { ProductCard } from "@/components/ui/productCard/ProductCard";
-import { BorderRadius, Spacing, Typography } from "@/constants/design-system";
+import {
+  BorderRadius,
+  Colors,
+  Spacing,
+  Typography,
+} from "@/constants/design-system";
 import { db } from "@/firebaseConfig";
 import { getPlantById } from "@/services/plantService";
 import { getUserProfile } from "@/services/userService";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Colors } from "@/constants/design-system";
 import {
   Image,
   Pressable,
@@ -87,40 +91,39 @@ export default function ViewPlantScreen() {
   }, [user?.uid]);
 
   return (
-    <>
-      <ScrollView style={styles.bgColor}>
-        <ProductCard
-          variant="view"
-          userId={user?.uid!}
-          plantId={id}
-          name={plantName}
-          description={description}
-          image={imageUrl}
-          readyToAdopt={readyToAdopt}
-          userLat={userProfileLat}
-          userLon={userProfileLon}
-          plantOwnerLat={plantOwnerLat}
-          plantOwnerLon={plantOwnerLon}
-        ></ProductCard>
-        <View style={styles.buttonContainer}>
-          <DefaultButton onPress={() => router.push(`/edit-plant/${plantId}`)}>
-            {userId === user?.uid ? "Ändra" : "Kontakta"}
-          </DefaultButton>
+    <ScrollView style={styles.bgColor}>
+      <ProductCard
+        variant="view"
+        userId={user?.uid!}
+        plantId={id}
+        name={plantName}
+        description={description}
+        image={imageUrl}
+        readyToAdopt={readyToAdopt}
+        userLat={userProfileLat}
+        userLon={userProfileLon}
+        plantOwnerLat={plantOwnerLat}
+        plantOwnerLon={plantOwnerLon}
+      ></ProductCard>
+      <View style={styles.buttonContainer}>
+        <DefaultButton onPress={() => router.push(`/edit-plant/${plantId}`)}>
+          {userId === user?.uid ? "Ändra" : "Kontakta"}
+        </DefaultButton>
+      </View>
+      {userId !== user?.uid ? (
+        <View style={styles.uploaderInfo}>
+          <Image
+            style={styles.profileImage}
+            source={{ uri: userProfileImageUrl }}
+          />
+          <Pressable onPress={() => router.push(`/view-profile/${userId}`)}>
+            <Text style={styles.uploaderName}>{userProfileName}</Text>
+          </Pressable>
         </View>
-        {userId !== user?.uid ? (
-          <View style={styles.uploaderInfo}>
-            <Image
-              style={styles.profileImage}
-              source={{ uri: userProfileImageUrl }}
-            />
-            <Pressable onPress={() => router.push(`/view-profile/${userId}`)}>
-              <Text style={styles.uploaderName}>{userProfileName}</Text>
-            </Pressable>
-          </View>
-        ) : (
-          " "
-        )}
-      </ScrollView>
+      ) : (
+        " "
+      )}
+    </ScrollView>
   );
 }
 
