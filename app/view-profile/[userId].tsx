4c21094424de9d4e654684e095a823a1b2241417
anna-dahlberg <anna.dahlberg@live.se>
2025-10-23 15:32:39 +0200

    added profile component to other users profile page

--- app/view-profile/[userId].tsx
+++ app/view-profile/[userId].tsx
@@ -1,40 +1,35 @@
 import { DefaultButton } from "@/components/ui/buttons/DefaultButton";
 import { ProductCard } from "@/components/ui/productCard/ProductCard";
-import { BorderRadius, Spacing, Styles } from "@/constants/design-system";
 import { getUserPlants } from "@/services/plantService";
 import { getUserProfile } from "@/services/userService";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { ProfileCard } from "../../components/ui/profilePage/ProfileCard";
 import { useAuth } from "../../contexts/AuthContext";
+import { UserProfile } from "../../interfaces/index";
 
 export default function ViewProfileScreen() {
   const router = useRouter();
   const { user } = useAuth();
   const { userId } = useLocalSearchParams<{ userId: string }>();
-  const [username, setUsername] = useState("");
-  const [bio, setBio] = useState("");
-  const [profileImageUrl, setProfileImageUrl] = useState("");
-  const [credits, setCredits] = useState(0);
+  const [userProfile, setUserProfile] = useState<Partial<UserProfile> | null>(
+    null
+  );
   const [plants, setPlants] = useState<any[]>([]);
   const [readyToAdoptPlants, setReadyToAdoptPlants] = useState<any[]>([]);
-  const [showAll, setShowAll] = useState(true)
+  const [showAll, setShowAll] = useState(true);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-
-
 
   useEffect(() => {
     if (userId) {
       async function fetchProfile() {
         try {
           setLoading(true);
-          const userProfile: any = await getUserProfile(userId);
-          if (userProfile) {
-            setUsername(userProfile.username || "");
-            setBio(userProfile.bio || "");
-            setProfileImageUrl(userProfile.profileImageUrl || "");
-            setCredits(userProfile.credits || 0);
+          const profile: any = await getUserProfile(userId);
+          if (profile) {
+            setUserProfile(profile);
           }
         } catch (err) {
           setError("Could not load profile");
@@ -54,7 +49,9 @@ export default function ViewProfileScreen() {
           const userPlants: any[] = await getUserPlants(userId);
           setPlants(userPlants);
 
-          const readyPlants = userPlants.filter(plant => plant.readyToAdopt === true);
+          const readyPlants = userPlants.filter(
+            (plant) => plant.readyToAdopt === true
+          );
           setReadyToAdoptPlants(readyPlants);
         } catch (err) {
           console.error("Error fetching plants:", err);
@@ -64,58 +61,54 @@ export default function ViewProfileScreen() {
     }
   }, [userId]);
 
-
-
   return (
     <ScrollView>
-      <View style={styles.profileInfo}>
-        <Image style={styles.profileImage} source={{ uri: profileImageUrl }} />
-        <View style={styles.profileWrapper}>
-        <View style={styles.profileText}>
-          <Text style={Styles.heading2}>{username}</Text>
-          <Text style={Styles.bodyXL}>Credits: {credits}</Text>
-          <Text style={Styles.bodyXS}>{bio}</Text>
-        </View>
-          <DefaultButton>Kontakta</DefaultButton>
-          <DefaultButton
-          onPress={() =>setShowAll(true)}>Alla plantor</DefaultButton>
-          <DefaultButton onPress={() =>setShowAll(false)}>Redo att adopteras</DefaultButton>
-        </View>
-      </View>
+      <ProfileCard userProfile={userProfile} />
 
+      <DefaultButton variant="secondary">Kontakta</DefaultButton>
 
-{showAll ?
-      <View style={styles.feed}>
-        {plants.map((plant) => (
-          <ProductCard
-            key={plant.id}
-            variant="small"
-            userId={user?.uid!}
-            plantId={plant.id}
-            name={plant.name}
-            description={plant.description}
-            image={plant.imageUrl}
-            readyToAdopt={plant.readyToAdopt}
-            onPress={() => router.push(`/view-plant/${plant.id}`)}
-          />
-        ))}
-      </View>
-:
-      <View style={styles.feed}>
-        {readyToAdoptPlants.map((plant) => (
-          <ProductCard
-            key={plant.id}
-            variant="small"
-            userId={user?.uid!}
-            plantId={plant.id}
-            name={plant.name}
-            description={plant.description}
-            image={plant.imageUrl}
-            readyToAdopt={plant.readyToAdopt}
-            onPress={() => router.push(`/view-plant/${plant.id}`)}
-          />
-        ))}
-      </View> }
+      {/* <View style={styles.buttonContainer}>
+        <DefaultButton onPress={() => setShowAll(true)}>
+          Alla plantor
+        </DefaultButton>
+        <DefaultButton onPress={() => setShowAll(false)}>
+          Redo att adopteras
+        </DefaultButton>
+      </View> */}
+
+      {showAll ? (
+        <View style={styles.feed}>
+          {plants.map((plant) => (
+            <ProductCard
+              key={plant.id}
+              variant="small"
+              userId={user?.uid!}
+              plantId={plant.id}
+              name={plant.name}
+              description={plant.description}
+              image={plant.imageUrl}
+              readyToAdopt={plant.readyToAdopt}
+              onPress={() => router.push(`/view-plant/${plant.id}`)}
+            />
+          ))}
+        </View>
+      ) : (
+        <View style={styles.feed}>
+          {readyToAdoptPlants.map((plant) => (
+            <ProductCard
+              key={plant.id}
+              variant="small"
+              userId={user?.uid!}
+              plantId={plant.id}
+              name={plant.name}
+              description={plant.description}
+              image={plant.imageUrl}
+              readyToAdopt={plant.readyToAdopt}
+              onPress={() => router.push(`/view-plant/${plant.id}`)}
+            />
+          ))}
+        </View>
+      )}
     </ScrollView>
   );
 }
@@ -127,24 +120,5 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly",
     marginBottom: 80,
   },
-  profileInfo: {
-    flexDirection: "row",
-    paddingHorizontal: Spacing.m,
-    paddingVertical: Spacing["2xl"],
-    gap: Spacing.m
-  },
-  profileImage: {
-    width: "47%",
-    aspectRatio: 1,
-    borderRadius: BorderRadius.xl,
-  },
-profileWrapper: {
-    height: 240,
-    gap: Spacing.m,
-    justifyContent: "space-between"
-},
-  profileText: {
-    gap: Spacing.m,
-    justifyContent: "flex-start",
-  },
+  contactButton: {},
 });
