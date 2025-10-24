d1e7efb4e8b9966e39e1df1f464360541b7ab99a
anna-dahlberg <anna.dahlberg@live.se>
2025-10-24 13:40:02 +0200

    Additional styling

--- components/ui/forms/FormLayoutComponent.tsx
+++ components/ui/forms/FormLayoutComponent.tsx
@@ -4,13 +4,18 @@ import { Dimensions, ScrollView, StyleSheet } from "react-native";
 
 interface FormLayoutProps {
   children: ReactNode;
+  centered?: boolean;
 }
 
 const { height } = Dimensions.get("window");
 
-export const FormLayout = ({ children }: FormLayoutProps) => {
+export const FormLayout = ({ children, centered = true }: FormLayoutProps) => {
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.content}>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={[styles.content, centered && styles.centered]}
+      showsVerticalScrollIndicator={false}
+    >
       {children}
     </ScrollView>
   );
@@ -22,10 +27,13 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.secondary,
   },
   content: {
+    paddingHorizontal: Spacing["2xl"],
+    paddingVertical: Spacing.xl,
+    gap: Spacing.m,
+  },
+  centered: {
     minHeight: height,
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: Spacing["2xl"],
-    gap: Spacing.m,
   },
 });
