2dae291a4f81cc3285070588481434bd5dd73f7d
Julia Lyngfelt <julialyngfelt@hotmail.com>
2025-10-27 12:07:44 +0100

    added refresh for profile page for both plants and profile (#184)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,12 +4,13 @@ import { getUserPlants } from "@/services/plantService";
 import { getUserProfile } from "@/services/userService";
 import { useRouter } from "expo-router";
 import { useEffect, useState } from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, RefreshControl } from "react-native";
 import { ProfileCard } from "../../components/ui/profilePage/profileCard";
 import { ProfileFeed } from "../../components/ui/profilePage/profileFeed";
 import { useAuth } from "../../contexts/AuthContext";
 import { Plant, UserProfile } from "../../interfaces/index";
 import TabLayout from "./_layout";
+
 
 export default function ProfileScreen() {
   const router = useRouter();
@@ -19,26 +20,30 @@ export default function ProfileScreen() {
     null
   );
   const [plants, setPlants] = useState<Plant[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const fetchPlants = async () => {
+    if (!user?.uid) return;
+    const userPlants = await getUserPlants(user.uid);
+    setPlants(userPlants);
+  };
+
+  const fetchProfile = async () => {
+    if (!user?.uid) return;
+    const profile = await getUserProfile(user.uid);
+    if (profile) {
+      setUserProfile(profile);
+    }
+  };
 
   useEffect(() => {
     if (user?.uid) {
-      async function fetchPlants() {
-        if (!user?.uid) return;
-        const userPlants = await getUserPlants(user?.uid);
-        setPlants(userPlants);
-      }
       fetchPlants();
     }
   }, [user?.uid]);
 
   useEffect(() => {
     if (user?.uid) {
-      async function fetchProfile() {
-        const profile = await getUserProfile(user?.uid!);
-        if (profile) {
-          setUserProfile(profile);
-        }
-      }
       fetchProfile();
     }
   }, [user?.uid]);
@@ -47,9 +52,24 @@ export default function ProfileScreen() {
     ? plants
     : plants.filter((plant) => plant.readyToAdopt);
 
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await Promise.all([fetchProfile(), fetchPlants()]); // Kör båda samtidigt
+    setRefreshing(false);
+  };
+
+  
   return (
     <>
-      <ScrollView style={styles.page}>
+      <ScrollView style={styles.page}
+      refreshControl={
+              <RefreshControl 
+                refreshing={refreshing} 
+                onRefresh={onRefresh}
+                colors={[Colors.accent]} 
+                tintColor={Colors.accent}
+              />
+            }>
         <ProfileCard userProfile={userProfile} />
         <FeedToggle showAll={showAll} onToggle={setShowAll} />
         <TabLayout />
