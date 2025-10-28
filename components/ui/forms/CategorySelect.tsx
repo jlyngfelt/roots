7b11c0ee22302b271a21a1cc61c8f7c3343de02d
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-10-28 09:42:34 +0100

    Based categoryselect on default select instead of rewriting it

--- components/ui/forms/CategorySelect.tsx
+++ components/ui/forms/CategorySelect.tsx
@@ -1,10 +1,7 @@
-import { Dropdown } from 'react-native-element-dropdown';
-import { StyleSheet } from 'react-native';
+import { DefaultSelect } from './DefaultSelect';
 import { useEffect, useState } from 'react';
 import { getCategories } from '@/services/categoryService';
 import { Category } from '@/interfaces';
-import { AntDesign } from '@expo/vector-icons';
-import { Colors, Styles, BorderRadius} from "../../../constants/design-system"
 
 export function CategorySelect({ 
   value, 
@@ -31,53 +28,11 @@ export function CategorySelect({
   }));
 
   return (
-    <Dropdown
-      style={styles.dropdown}
-      placeholderStyle={[Styles.bodyM, styles.placeholderStyle]}
-      selectedTextStyle={[Styles.bodyM, styles.selectedTextStyle]}
-      containerStyle={styles.containerStyle}
-      itemTextStyle={[Styles.bodyM, styles.itemTextStyle]}
-      itemContainerStyle={styles.itemContainerStyle} 
-      activeColor="#f0f0f0"
-      data={data}
-      labelField="label"
-      valueField="value"
-      placeholder={placeholder}
+    <DefaultSelect 
       value={value}
-      onChange={item => onValueChange(item.value)}
-      renderRightIcon={() => (
-        <AntDesign name="down" size={15} color={Colors.details} />
-      )}
+      onValueChange={onValueChange}
+      data={data}
+      placeholder={placeholder}
     />
   );
 }
-
-const styles = StyleSheet.create({
-  dropdown: {
-    width: "100%",
-    height: 46,
-    borderWidth: 1,
-    borderColor: Colors.details,
-    borderRadius: BorderRadius.m,
-    paddingHorizontal: 24,
-    backgroundColor: Colors.secondary,
-  },
-  placeholderStyle: {
-    color: Colors.details,
-  },
-  selectedTextStyle: {
-    color: Colors.details,
-  },
-  containerStyle: {
-    borderRadius: BorderRadius.m,
-    borderWidth: 1,
-    borderColor: Colors.light,
-    marginTop: 4,
-  },
-  itemTextStyle: {
-    color: Colors.details,
-  },
-  itemContainerStyle: {
-    borderRadius: BorderRadius.m, 
-  }
-});
