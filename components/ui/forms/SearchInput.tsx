2dccabd6e4150f168982649154154940e996078e
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-10-30 11:53:10 +0100

    Updated styling in component

--- components/ui/forms/SearchInput.tsx
+++ components/ui/forms/SearchInput.tsx
@@ -1,8 +1,8 @@
 import { IconSymbol } from "@/components/ui/icon-symbol";
-import { Colors, Styles } from "@/constants/design-system";
+import { Colors, Spacing, Styles } from "@/constants/design-system";
+import { SearchInputProps } from "@/interfaces";
 import { useEffect, useState } from "react";
 import { Pressable, StyleSheet, TextInput, View } from "react-native";
-import { SearchInputProps } from "@/interfaces";
 
 export const SearchInput = ({
   value,
@@ -12,9 +12,7 @@ export const SearchInput = ({
 }: SearchInputProps) => {
   const [localValue, setLocalValue] = useState(value);
 
-
-
-// Debounce
+  // Debounce
   useEffect(() => {
     const timer = setTimeout(() => {
       onChangeText(localValue);
@@ -23,8 +21,8 @@ export const SearchInput = ({
     return () => clearTimeout(timer);
   }, [localValue, debounceMs]);
 
-// synka om
-    useEffect(() => {
+  // synka om
+  useEffect(() => {
     setLocalValue(value);
   }, [value]);
 
@@ -33,13 +31,12 @@ export const SearchInput = ({
     onChangeText("");
   };
 
-    return (
+  return (
     <View style={styles.container}>
-      {/* Sök-ikon till vänster */}
-      <IconSymbol 
-        name="magnifyingglass" 
-        size={20} 
-        color={Colors.details} 
+      <IconSymbol
+        name="magnifyingglass"
+        size={20}
+        color={Colors.details}
         style={styles.searchIcon}
       />
 
@@ -53,11 +50,7 @@ export const SearchInput = ({
 
       {localValue.length > 0 && (
         <Pressable onPress={handleClear} style={styles.clearButton}>
-          <IconSymbol 
-            name="xmark.circle.fill" 
-            size={20} 
-            color={Colors.details} 
-          />
+          <IconSymbol name="xmark.circle" size={20} color={Colors.details} />
         </Pressable>
       )}
     </View>
@@ -68,25 +61,29 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center",
-    borderWidth: 1,
+    borderBottomWidth: 1,
     borderColor: Colors.details,
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    paddingVertical: 8,
+    paddingHorizontal: Spacing.m,
+    paddingVertical: 12,
     backgroundColor: "transparent",
     width: "100%",
+    position: "relative",
   },
   searchIcon: {
-    marginRight: 8,
+    marginRight: Spacing.s,
+    paddingVertical: 0,
+    marginVertical: 0,
   },
   input: {
     flex: 1,
     ...Styles.bodyM,
     color: Colors.details,
-    padding: 0, // Ta bort default padding
+    padding: 0,
   },
   clearButton: {
-    padding: 4,
-    marginLeft: 8,
+    padding: Spacing.xs,
+    marginLeft: Spacing.s,
+    position: "absolute",
+    right: 12,
   },
 });
