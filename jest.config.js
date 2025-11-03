5a86a2b39d2c54727f83c23acbb4e9f9e7a62ad4
Anna Dahlberg <anna.dahlberg@live.se>
2025-11-03 12:47:16 +0100

    Feature testing setup (#272)

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: "jest-expo",
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|firebase)",
+  ],
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jest.setup.js",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+};
